031af11dca836497c28099e1e72cdae4e86153d7
1heart <yixin1996@gmail.com>
2014-08-04 11:54:05 -0700

    questions 1 and 2

--- scheme_reader.py
+++ scheme_reader.py
@@ -49,44 +49,54 @@ def scheme_read(src):
             return intern(val)
     elif val == "'":
         "*** YOUR CODE HERE ***"
+        return Pair('quote', Pair(scheme_read(src), nil))
     elif val == "(":
         return read_tail(src)
     else:
         raise SyntaxError("unexpected token: {0}".format(val))
 
 def read_tail(src):
-    """Return the remainder of a list in SRC, starting before an element or ).
+	"""Return the remainder of a list in SRC, starting before an element or ).
 
-    >>> read_tail(Buffer(tokenize_lines([")"])))
-    nil
-    >>> read_tail(Buffer(tokenize_lines(["2 3)"])))
-    Pair(2, Pair(3, nil))
-    >>> read_tail(Buffer(tokenize_lines(["2 (3 4))"])))
-    Pair(2, Pair(Pair(3, Pair(4, nil)), nil))
-    >>> read_line("(1 . 2)")
-    Pair(1, 2)
-    >>> read_line("(1 2 . 3)")
-    Pair(1, Pair(2, 3))
-    >>> read_line("(1 . 2 3)")
-    Traceback (most recent call last):
-        ...
-    SyntaxError: Expected one element after .
-    >>> scheme_read(Buffer(tokenize_lines(["(1", "2 .", "'(3 4))", "4"])))
-    Pair(1, Pair(2, Pair('quote', Pair(Pair(3, Pair(4, nil)), nil))))
+	>>> read_tail(Buffer(tokenize_lines([")"])))
+	nil
+	>>> read_tail(Buffer(tokenize_lines(["2 3)"])))
+	Pair(2, Pair(3, nil))
+	>>> read_tail(Buffer(tokenize_lines(["2 (3 4))"])))
+	Pair(2, Pair(Pair(3, Pair(4, nil)), nil))
+	>>> read_line("(1 . 2)")
+	Pair(1, 2)
+	>>> read_line("(1 2 . 3)")
+	Pair(1, Pair(2, 3))
+	>>> read_line("(1 . 2 3)")
+	Traceback (most recent call last):
+		...
+	SyntaxError: Expected one element after .
+	>>> scheme_read(Buffer(tokenize_lines(["(1", "2 .", "'(3 4))", "4"])))
+	Pair(1, Pair(2, Pair('quote', Pair(Pair(3, Pair(4, nil)), nil))))
 
-    """
-    try:
-        if src.current() is None:
-            raise SyntaxError("unexpected end of file")
-        if src.current() == ")":
-            src.pop()
-            return nil
-        "*** YOUR CODE HERE ***"
-        first = scheme_read(src)
-        rest = read_tail(src)
-        return Pair(first, rest)
-    except EOFError:
-        raise SyntaxError("unexpected end of file")
+	"""
+	try:
+		if src.current() is None:
+			raise SyntaxError("unexpected end of file")
+		if src.current() == ")":
+			src.pop()
+			return nil
+		"*** YOUR CODE HERE ***"
+		if src.current() == ".":
+			src.pop()
+			first = scheme_read(src)
+			if src.pop()  == ")":
+				return first
+			else:
+				raise SyntaxError
+		else:
+			first = scheme_read(src)
+			rest = read_tail(src)
+			print(first, rest)
+			return Pair(first, rest)
+	except EOFError:
+		raise SyntaxError("unexpected end of file")
 
 # Convenience methods
 
